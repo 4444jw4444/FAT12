1fe74d502c8bbadd0a06b948e5356f90e47317f4
Jorin Weatherston <jorinw@1.linux.csc.uvic.ca>
2015-11-24 17:56:37 -0800

    Reading in the OSLine on getinfo

--- diskinfo.c
+++ diskinfo.c
@@ -24,11 +24,23 @@ int main(int argc, char* argv[]){
     	printf("ERROR:File: %s unavailable or corrupt, please specifiy a valid file\n", argv[1]);
     	return -1;
     }
+    fseek(fo, 3, SEEK_SET);
+    //alloc an object to hold the recieved input from the disk image
+    char OSLine[9];
+    //read in a chunk of data
+    fread(OSLine,8,1,fo);
+    //add the string terminator
+    OSLine[8] = '\0';
+    //display the Operating system
+    printf("OS Name: %s\n", OSLine);
     //close the file stream
     fclose(fo);
     //exit successfully
     printf("Success:Exiting\n");
+    return 0;
+
 }
 //fread
 //fseek
 //fopen, read-binary
+ 
